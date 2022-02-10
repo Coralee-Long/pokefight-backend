cd89620b54f633f1368ed6385a03c7ff56375512
kavitatandel <kavita2810@gmail.com>
2022-02-10 14:53:26 +0100

    added authorization and routers

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const pokeRouter = require("./routes/pokeRouter");
 const app = express();
+require('dotenv').config()
+const mongoose = require('mongoose')
 const PORT = process.env.PORT || 5000;
+//use cors to communicate between front-end and back-end
+const cors = require("cors");
+app.use(cors());
+const userRouter = require("./routes/userRouter");
+const authRouter = require("./routes/authRouter");
+
 
 //it will convert response to json
 app.use(express.json());
@@ -13,10 +21,13 @@ app.use(
   })
 );
 
-//use cors to communicate between front-end and back-end
-const cors = require("cors");
-app.use(cors());
+mongoose.connect(process.env.MONGO_DB)
+
 
 app.use("/pokemon", pokeRouter);
+
+app.use("/users", userRouter)
+
+app.use("/", authRouter)
 
 app.listen(PORT, console.log(`Server is listening on port ${PORT}`));
